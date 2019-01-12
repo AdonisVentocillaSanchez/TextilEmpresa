0a84c64102f3c02bc9f53c87bea787418bf4c67e
AdonisVentocillaSanchez <41137015+AdonisVentocillaSanchez@users.noreply.github.com>
2019-01-12 13:28:31 -0500

    extra

---
+++ bats/backup_schema.bat
@@ -0,0 +1,34 @@
+REM .bat con permisos de administrador
+:-------------------------------------
+REM  --> Analizando los permisos
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> Si hay error es que no hay permisos de administrador.
+if '%errorlevel%' NEQ '0' (
+    echo Solicitando permisos de administrador... Requesting administrative privileges... Anfordern Administratorrechte ...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------  
+
+REM  CODIGO PARA BACKUP EN ORACLE
+mkdir C:\respaldos
+exp userid=system/admin owner=ventocilla buffer=4096 file=C:\respaldos\%date:~5,2%%date:~8,2%%date:~11,4%.dmp log=C:\respaldos\%date:~5,2%%date:~8,2%%date:~11,4%.log
+timeout -1
+EXIT
